e84305b095ad419cc33267389d62c5c70a1ea4dd
reshetnev <a.v.reshetnev@gmail.com>
2016-02-19 23:02:07 +0200

    Updated LoginController

--- src/main/java/com/epam/reshetnev/spring/advanced/controller/LoginController.java
+++ src/main/java/com/epam/reshetnev/spring/advanced/controller/LoginController.java
@@ -3,17 +3,27 @@ package com.epam.reshetnev.spring.advanced.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView handleLogin() {
+    public ModelAndView handleLogin(
+        @RequestParam(value = "error", required = false) String error,
+        @RequestParam(value = "logout", required = false) String logout) {
+
         ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username or password!");
+        }
+
+        if (logout != null) {
+            model.addObject("message", "You have been logged out successfully.");
+        }
         model.setViewName("login");
+
         return model;
     }
-
-    
 }
