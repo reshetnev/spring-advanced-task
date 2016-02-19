432b6b584322cf51780831df724b9906b1434b9b
Aleksandr Reshetnev <a.v.reshetnev@gmail.com>
2016-02-19 17:03:35 +0300

    Added Spring Security

---
+++ src/main/java/com/epam/reshetnev/spring/advanced/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.epam.reshetnev.spring.advanced.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView handleLogin() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("login");
+        return model;
+    }
+
+    
+}
